75bed7e9dba54c171bf9036b64f02210203a647f
Cristian Luis <alu0100693386@ull.edu.es>
2014-03-27 15:44:17 +0000

    Pruebas 8/10

--- public/tests.js
+++ public/tests.js
@@ -38,7 +38,6 @@ suite( 'Analizador sintáctico con PEGJS', function(){  //Suite equivale al desc
   });
   
   test('Error gramatico', function(){  
-    var result = pl0.parse("var i = 0, u = 9.");
     assert.throws(pl0.parse("var i = 0, u = 9."));
   });
   
